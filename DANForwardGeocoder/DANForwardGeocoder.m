7e0d5ef5ea5066270c69d67f3d1baa83ae9a2534
Daniel Tomlinson <Dan.Tomlinson@wearebase.com>
2014-06-03 18:09:30 +0100

    URL String generation

--- DANForwardGeocoder/DANForwardGeocoder.m
+++ DANForwardGeocoder/DANForwardGeocoder.m
@@ -8,6 +8,84 @@
 
 #import "DANForwardGeocoder.h"
 
+#pragma mark - Private Categories
+
+#pragma mark Coordinate Bounds to String
+
+@interface DANForwardGeocoderCoordinateBounds (NSStringRepresentation)
+/**
+ *  NSString representing the bounding box with the format "(southWest latitude),(southWest longitude)|(northEast latitude),(northEast longitude)"
+ */
+@property (nonatomic, readonly) NSString *boundsString;
+
+@end
+
+@implementation DANForwardGeocoderCoordinateBounds (NSStringRepresentation)
+
+- (NSString *)boundsString
+{
+    return [NSString stringWithFormat:@"%f,%f|%f,%f", self.southWest.latitude, self.southWest.longitude, self.northEast.latitude, self.northEast.longitude];
+}
+
+@end
+
+#pragma mark String encoding
+
+@interface NSString (DANFG_ENCODING)
+@property (nonatomic, readonly) NSString *danfg_urlEncodedString;
+@end
+
+@implementation NSString (DANFG_ENCODING)
+
+- (NSString *)danfg_urlEncodedString
+{
+    return [self stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+}
+
+@end
+
+#pragma mark - DANForwardGeocoder
+
+@interface DANForwardGeocoder ()
+@property (nonatomic, copy) DANForwardGeocoderSuccess successBlock;
+@property (nonatomic, copy) DANForwardGeocoderFailed failureBlock;
+@end
+
 @implementation DANForwardGeocoder
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+    }
+    
+    return self;
+}
+
+- (void)forwardGeocodeWithQuery:(DANForwardGeocoderQuery *)query
+                        success:(DANForwardGeocoderSuccess)success
+                        failure:(DANForwardGeocoderFailed)failure
+{
+    
+    
+}
+
+#pragma mark - Private Methods
+
+- (NSString *)URLStringForQuery:(DANForwardGeocoderQuery *)query
+{
+    NSMutableString *geocodedURLString = [NSString stringWithFormat:@"%@://maps.google.com/maps/api/geocode/xml?address=%@&sensor=false", (self.useHTTP ? @"http" : @"https"), query.searchQuery.danfg_urlEncodedString].mutableCopy;
+    
+    if (query.regionBias && ![query.regionBias isEqualToString:@""]) {
+        [geocodedURLString appendFormat:@"&region=%@", query.regionBias];
+    }
+    
+    if (query.viewportBias) {
+        NSString *boundsString = query.viewportBias.boundsString.danfg_urlEncodedString;
+        [geocodedURLString appendFormat:@"&bounds=%@", boundsString];
+    }
+    
+    return geocodedURLString.copy;
+}
 
 @end
