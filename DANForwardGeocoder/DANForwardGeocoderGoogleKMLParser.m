79c19096ba736d6665e121433afaa9ca39950d1a
Daniel Tomlinson <Dan.Tomlinson@wearebase.com>
2014-06-03 18:24:22 +0100

    Add KML Parser and Result

---
+++ DANForwardGeocoder/DANForwardGeocoderGoogleKMLParser.m
@@ -0,0 +1,13 @@
+//
+//  DANForwardGeocoderGoogleKMLParser.m
+//  DANForwardGeocoder
+//
+//  Created by Daniel Tomlinson on 03/06/2014.
+//  Copyright (c) 2014 Daniel Tomlinson. All rights reserved.
+//
+
+#import "DANForwardGeocoderGoogleKMLParser.h"
+
+@implementation DANForwardGeocoderGoogleKMLParser
+
+@end
---
+++ DANForwardGeocoder/DANForwardGeocoderKMLResult.m
@@ -0,0 +1,13 @@
+//
+//  DANForwardGeocoderKMLResult.m
+//  DANForwardGeocoder
+//
+//  Created by Daniel Tomlinson on 03/06/2014.
+//  Copyright (c) 2014 Daniel Tomlinson. All rights reserved.
+//
+
+#import "DANForwardGeocoderKMLResult.h"
+
+@implementation DANForwardGeocoderKMLResult
+
+@end
